9515ebe4860fb9f0b4ada0cc41d958a45b3c7836
Eddie Hurtig <eddie@hurtigtechnologies.com>
2015-05-02 00:09:27 -0400

    Bump top version 0.1.0

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'eddie@hurtigtechnologies.com'
 license          'apache2'
 description      'Installs/Configures gogs'
 long_description 'Installs/Configures gogs'
-version          '0.0.1'
+version          '0.1.0'
 
 supports 'ubuntu', '>= 12.04'
 
