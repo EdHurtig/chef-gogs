cdb71c8b54d43432b66a073917ef2de56c0493b9
Eddie Hurtig <EdHurtig@users.noreply.github.com>
2015-05-02 13:10:33 -0400

    Describe the cookbook better

--- metadata.rb
+++ metadata.rb
@@ -2,8 +2,8 @@ name             'gogs'
 maintainer       'Eddie Hurtig'
 maintainer_email 'eddie@hurtigtechnologies.com'
 license          'apache2'
-description      'Installs/Configures gogs'
-long_description 'Installs/Configures gogs'
+description      'Installs and Configures a Go Git Service Server'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 
 supports 'ubuntu', '>= 12.04'
