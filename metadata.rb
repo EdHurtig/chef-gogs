76db846ea9ab566885fb4ac4dd7162c6582b1be8
Eddie Hurtig <eddie@hurtigtechnologies.com>
2015-05-02 13:12:21 -0400

    Bump Version 0.1.1

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'eddie@hurtigtechnologies.com'
 license          'apache2'
 description      'Installs and Configures a Go Git Service Server'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '0.1.1'
 
 supports 'ubuntu', '>= 12.04'
 
